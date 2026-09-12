3b326c5406ce1530f23dfe1a691a7eef0a409750
Hyvka <anfibia.zloy@mail.ru>
2026-09-12 07:16:31 +0000

    123

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,13 @@
 import Sidebar from './Sidebar'
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
         <div className="app">
             <Sidebar />
+            <div className="main">
+                <Outlet />
+            </div>
         </div>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { NavLink, useNavigation } from "react-router-dom";
+
 const Sidebar = () => {
     return (
         <aside className="sidebar">
@@ -10,14 +12,14 @@ const Sidebar = () => {
             </div>
             <nav className="navigation">
                 <div className="nav-section-title">Библиотека</div>
-                <a className="nav-item active" href="index.html">
+                <NavLink className={({ isActive }) => `nav-item${isActive ? " active" : ""}`} to="index.html">
                     <span>⌂</span>
                     Обзор
-                </a>
-                <a className="nav-item" href="favorites.html">
+                </NavLink>
+                <NavLink className={({ isActive }) => `nav-item${isActive ? " active" : ""}`} to="favorites.html">
                     <span>♡</span>
                     Избранное
-                </a>
+                </NavLink>
             </nav>
             <div className="sidebar-bottom">
                 <div className="profile">
